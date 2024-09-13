48afd0c4e003784848f850958e70defb4007ca76
ThuHoiBao <166045722+ThuHoiBao@users.noreply.github.com>
2024-09-13 09:22:00 +0700

    Thuuu

--- js/main.js
+++ js/main.js
@@ -3,6 +3,16 @@ const menu = document.querySelector(".navbar__links");
 const menuButton = document.querySelector(".navbar__icons");
 const overlay = document.querySelector("#overlay");
 
+
+document.addEventListener("DOMContentLoaded", function () {
+  var typed = new Typed(".text", {
+    strings: ["I'm student", "I'm Anh Thu"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
+});
 menuButton.addEventListener("click", () => {
   menu.classList.toggle("navbar__open");
   menuButton.classList.toggle("open");
